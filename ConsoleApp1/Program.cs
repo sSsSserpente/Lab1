cd56c6a39ee7cdd0790f3239e748e4bdec906510
{{sSsSserpente}} <{{kiruxazmey@gmail.com}}>
2023-09-08 13:10:11 +0300

    second commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("You shot me then you got me 'nd I'm like damn, rompompom");
